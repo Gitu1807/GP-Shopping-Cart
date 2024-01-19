cc2e9d321868966612231087924ffb58fde7b925
Gitesh Paitwar <123587252+Gitu1807@users.noreply.github.com>
2024-01-19 20:10:22 +0530

    Add files via upload

---
+++ ecommapp/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Product(models.Model):
+    CAT=((1,'Mobile'),(2,'Shoes'),(3,'Laptop'))
+    
+    name=models.CharField(max_length=50, verbose_name='Product Name')
+    print=models.FloatField()
+    cat=models.IntegerField(max_length=50, verbose_name='Category', choices=CAT)
+    pdetail=models.CharField(max_length=300, verbose_name='Product Detail')
+    is_active=models.BooleanField(default=True)
+    pimage=models.ImageField(upload_to='image')
+
+    
+    '''def __str__(self):
+       return self.name'''
+    
+class Cart(models.Model):
+    uid=models.ForeignKey('auth.User',on_delete=models.CASCADE,db_column='uid')
+    pid=models.ForeignKey('Product',on_delete=models.CASCADE,db_column='pid')
+    qty=models.IntegerField(default=1)
+    
+'''class Order(models.Model):
+    orderid=models.IntegerField()
+    uid=models.ForeignKey('auth.User',on_delete=models.CASCADE,db_column='uid')
+    pid=models.ForeignKey('Product',on_delete=models.CASCADE,db_column='pid')
+    qty=models.IntegerField(default=1)'''
+    
+
+class OrderA(models.Model):
+    orderid=models.IntegerField()
+    uid=models.ForeignKey('auth.User',on_delete=models.CASCADE,db_column='uid')
+    pid=models.ForeignKey('Product',on_delete=models.CASCADE,db_column='pid')
+    qty=models.IntegerField(default=1)
+    amt=models.FloatField()  
+
+
+    
+
+    
